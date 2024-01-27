5d81f1be81c2a16a7acc1429b6ab88cc10c34f6d
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-27 22:49:43 +0530

    styling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,60 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Feature from "@/components/Feature/Feature";
+
 const page = () => {
+  const listItems = [
+    { name: "Home", link: "/" },
+    { name: "Contact Us", link: "/contact" },
+    { name: "About Us", link: "/about" },
+    { name: "Login", link: "/login" },
+    { name: "Signup", link: "/signup" },
+  ];
+
   return (
     <div>
       {/* we are going to make navbar */}
-      <div>
-        <ul>
+      <div className="flex flex-row w-screen border-b-[1px] border-black shadow-lg p-3 bg-[#FFC8DD] ">
+        <ul className="flex flex-row items-center  justify-between w-full ">
           <Link href="">
-            <li></li>
+            <li>
+              <Image
+                src="/logo-only-removebg-preview.png"
+                width={50}
+                height={50}
+                alt="appoint-next-logo"
+              />
+            </li>
           </Link>
-          <Link href="">
-            <li></li>
-          </Link>
-          <Link href="">
-            <li></li>bs
-          </Link>
-          <Link href="">
-            <li></li>
-          </Link>
-          <Link href="">
-            <li></li>
-          </Link>
+          {listItems.map((item, index) => (
+            <Link href={item.link} key={index}>
+              <li className="hover:font-bold transition-all w-[150px]">
+                {item.name}
+              </li>
+            </Link>
+          ))}
         </ul>
       </div>
+      <div
+        className="h-screen w-screen bg-[#ffffff] flex flex-row items-center justify-center border-b-[1px] border-black shadow-2xl
+      "
+      >
+        <div className="p-40">Book Appointment like never before</div>
+        <div>
+          <Image
+            className=""
+            src="/hero-bg.png"
+            width={window.innerWidth}
+            height={window.innerHeight}
+            alt="Picture of the author"
+          />
+        </div>
+      </div>
+      <h1>hello</h1>
+      <Feature />
     </div>
   );
 };
