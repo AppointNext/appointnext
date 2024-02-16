eaddcdb6773344a5ca4ce92aab2983117adba9c5
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-02-17 00:28:17 +0530

    adding backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import DrSignup from './components/DrSignup'
+import "./App.css";
+import DrSignup from "./components/DrSignup";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <DrSignup/>
+      <DrSignup />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
