53a2d315a2030facbe5f1bab75fdb4caebb3370c
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-02-17 22:01:20 +0530

    adding landing page hero and features

--- client/src/components/Feature.jsx
+++ client/src/components/Feature.jsx
@@ -4,15 +4,16 @@ import { FaUser } from "react-icons/fa";
 const Feature = () => {
   return (
     <div className="px-40 my-8 py-12 shadow-2xl shadow-gray-300 flex flex-row justify-between items-center rounded-lg">
-      <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-row items-center justify-between w-[190px]">
         <FaUser />
         <p>Manage Patients</p>
       </div>
       <div className="flex flex-row items-center justify-between">
-        <FaUser />
+        <FaUser className="bg-[#C2D0F5] rounded-full h-10 w-10 text-[#003cd8] p-2" />
+
         <p>Manage Patients</p>
       </div>
-      <div className="flex flex-row items-center justify-between ">
+      <div className="flex flex-row items-center justify-between w-[190px] ">
         <FaUser />
         <p>Manage Patients</p>
       </div>
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className=" flex flex-row w-screen items-center">
       <div className="w-1/2">
         <p>hello this appointment management app</p>
-        <button className="bg-[#003cd8] text-white py-4 px-10 shadow-2xl shadow-[#003cd8] rounded-xl ">
+        <button className="bg-[#003cd8] text-white py-4 px-10 hover:shadow-2xl hover:shadow-[#003cd8] rounded-xl transition-all active:translate-y-3 ">
           Get Started
         </button>
       </div>
