a7384ac7927c1f79db793907c57398414a068a9d
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-02-24 10:54:01 +0530

    making responsive

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className=" flex flex-row w-screen items-center overflow-hidden">
-      <div className="w-1/2">
+      <div className="w-1/2 ">
         <p>hello this appointment management app</p>
         <button className="bg-[#003cd8] text-white py-4 px-10 hover:shadow-2xl hover:shadow-[#003cd8] rounded-xl transition-all active:translate-y-3 ">
           Get Started
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-between py-8 md:block ">
+      <div className="flex flex-row items-center justify-between py-8 ">
         <div className="flex flex-row items-center cursor-pointer w-[20px]">
           <img src="./logo.png" alt="logo" className="h-[50px]" />
           <p className="inline md:text-[24px] text-[10px] md:pl-2">
@@ -28,7 +28,7 @@ const Navbar = () => {
           </p>
         </div>
         <div>
-          <ul className="*:flex flex-row justify-between xl:sm:w-screen w-[400px] md:w-screen 2xl:w-[500px] text-[16px] border-2 border-white h-[35px] transition-colors hidden ">
+          <ul className="sm:md:flex flex-row justify-between xl:sm:w-screen w-[400px] md:w-screen 2xl:w-[500px] text-[16px] border-2 border-white h-[35px] transition-colors hidden">
             <li className="hover:border-b-2 border-[#003cd8] cursor-pointer">
               About
             </li>
@@ -47,11 +47,13 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="flex justify-between items-center w-100 flex-row gap-1 cursor-pointer">
-          <button className="px-10 rounded-2xl py-2">Sign In</button>
+          <button className="md:px-10 rounded-2xl md:py-2 md:text-[50px] text-[15px] px-4 py-1">
+            Sign In
+          </button>
           <button
-            className={`border-[#003CD8] border-2 px-10 text-[#003CD8] rounded-2xl py-2 hover:shadow-2xl active:translate-y-2 transition-all ${
+            className={`border-[#003CD8] border-2 md:px-10 text-[#003CD8] rounded-2xl md:py-2 hover:shadow-2xl active:translate-y-2 transition-all ${
               isAnimating ? "active:delay-75" : ""
-            }`}
+            } md:text-[50px] text-[15px] px-4 py-1`}
             onClick={handleSignUpClick}
           >
             Sign Up
