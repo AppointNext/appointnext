353f708a23470aec5f33703cb009a9db5c8e2af9
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-03-01 22:23:29 +0530

    started implementing location feature

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <p className=" sm:text-[15px] text-[15px] ">
           hello this appointment management app
         </p>
-        <button className="bg-[#003cd8] text-white xl:py-4 xl:px-10 px-2 py-1 hover:shadow-2xl hover:shadow-[#003cd8] rounded-xl transition-all active:translate-y-3 ">
+        <button className="bg-[#003cd8] text-white xl:py-4 xl:px-10 px-2 py-1 hover:shadow-2xl hover:shadow-[#003cd8] rounded-xl transition-all active:translate-y-3 font-bold ">
           Get Started
         </button>
       </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Feature from "../components/Feature";
 
 const Home = () => {
+  const [location, setLocation] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const { coords } = await getCurrentLocation();
+      const { latitude, longitude } = coords;
+      setLocation({ latitude, longitude });
+    } catch (error) {
+      console.error("Error searching:", error);
+    }
+  };
+  const getCurrentLocation = async () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+  handleSearch();
+  console.log(location);
   return (
     <div className="md:px-14 px-2">
       <Navbar />
