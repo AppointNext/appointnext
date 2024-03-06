79e6e51ff103e01ed8ef29af5d2e286f58f3482a
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-03-06 18:26:23 +0530

    adding more page overview,task,etc

--- client/src/components/Dashboard/Nav.jsx
+++ client/src/components/Dashboard/Nav.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-// import { useSelector } from "react-redux";
 import { useCookies } from "react-cookie";
 
 const Nav = () => {
   const [, , removeCookie] = useCookies(["refreshToken", "accessToken"]);
-
   const navigate = useNavigate();
-  // const userData = useSelector((state) => state.auth.userData);
   const username = localStorage.getItem("username");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
+  };
+  const toggleDropdown2 = () => {
+    setIsDropdownOpen2(!isDropdownOpen2);
   };
 
   const handleLogout = async () => {
@@ -33,31 +34,65 @@ const Nav = () => {
     <div className="py-4">
       <nav className="flex justify-between w-screen items-center px-4">
         <div className="flex justify-center items-center px-2 w-[1/2]">
-          <div>
-            <i className="fa-solid fa-bars"></i>
-          </div>
+          <i
+            className="fa-solid fa-bars cursor-pointer"
+            onClick={toggleDropdown}
+          ></i>
+          {isDropdownOpen && (
+            <div className="absolute left-2 top-10 mt-2 w-32 bg-white rounded-lg shadow-xl z-10">
+              <div className="py-1">
+                <NavLink
+                  to="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px] active:bg-indigo-500 "
+                >
+                  Overview
+                </NavLink>
+                <NavLink
+                  to="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px]"
+                >
+                  Task
+                </NavLink>
+                <NavLink
+                  to="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px]"
+                >
+                  Mentor
+                </NavLink>
+                <NavLink
+                  to="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px]"
+                >
+                  Message
+                </NavLink>
+                <NavLink
+                  to="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px]"
+                >
+                  Setting
+                </NavLink>
+              </div>
+            </div>
+          )}
         </div>
         <div className="flex justify-center items-center full gap-2">
-          <div>
-            <i className="fa-solid fa-bell"></i>
-          </div>
-
+          <i className="fa-solid fa-bell"></i>
           <div className="relative">
             <img
               className="rounded-full w-10 h-10 cursor-pointer"
               src="./profile.png"
               alt="profile"
-              onClick={toggleDropdown}
+              onClick={toggleDropdown2}
             />
-            {isDropdownOpen && (
+            {isDropdownOpen2 && (
               <div className="absolute right-0 mt-2 w-32 bg-white rounded-lg shadow-xl z-10">
                 <div className="py-1">
-                  <Link
-                    href="#"
+                  <NavLink
+                    to="#"
                     className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px]"
                   >
                     Settings
-                  </Link>
+                  </NavLink>
                   <button
                     className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white text-[14px]"
                     onClick={handleLogout}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,34 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Appointment from "../components/Dashboard/Appointment";
 import Nav from "../components/Dashboard/Nav";
 import Hero from "../components/Dashboard/Hero";
 import { useSelector } from "react-redux";
 import Calendar from "../components/Dashboard/Calender";
+import { Routes,Route } from "react-router-dom";
 
 const Dashboard = () => {
+  // 1. Overveiw
+  // 2. Task
+  // 3. Mentor
+  // 4. Message
+  // 5. Setting
+  const [page, setPage] = useState(1);
   return (
-    <div className="flex flex-col gap-2">
-      <Nav />
-      <Hero />
-      <div className="flex flex-col gap-2 rounded-lg border-2 p-1 border-slate-200">
-        <div className=" my-3 flex justify-between px-2">
-          <h1 className=" font-semibold">Upcoming Appointments</h1>
-          <div className="flex gap-2 items-center">
-            <i className="fa-solid fa-angle-left"></i>
-            <i className="fa-solid fa-angle-right"></i>
-          </div>
-        </div>
-        <Appointment />
-      </div>
-      <div className="border-2 p-1 border-slate-200">
-        <Calendar />
-      </div>
-      <div>
-        <Appointment />
-      </div>
-      <br />
-      <br />
-    </div>
+    <Routes>
+      <Route path="/overview" element={<}/>
+    </Routes>
   );
 };
 
