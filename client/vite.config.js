f16f5362fc35444e73a36f81574033ec33f08aab
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-03-14 19:33:24 +0530

    login done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
-
-// Get the directory name of the current file
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default defineConfig({
-  https: {
-    key: fs.readFileSync(
-      path.resolve(
-        __dirname,
-        "/Users/latishadwani/Developer/Webdev/Projects/Appoint-next/client/key.pem"
-      )
-    ),
-    cert: fs.readFileSync(
-      path.resolve(
-        __dirname,
-        "/Users/latishadwani/Developer/Webdev/Projects/Appoint-next/client/cert.pem"
-      )
-    ),
-  },
   plugins: [react()],
 });
