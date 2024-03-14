cec1119278f86c21fcb4a46b49e82c211a7f9fe9
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-03-14 15:21:18 +0530

    login done

--- client/src/_root/RootLayout.jsx
+++ client/src/_root/RootLayout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Sidebar from "../components/ui/Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import Cookie from "js-cookie";
 const RootLayout = () => {
+  const refreshToken = Cookie.get("refreshToken");
+  const accessToken = Cookie.get("accessToken");
+
+  if (!refreshToken && !accessToken) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <div className="">
       <Sidebar />
