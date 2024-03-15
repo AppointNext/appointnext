260c2954dcdd2850dba318718c5f21783227e661
Vineetchelani1304 <vineetchelani545@gmail.com>
2024-03-16 00:22:15 +0530

    dr signup update

--- client/src/_auth/forms/DoctorSignupForm.jsx
+++ client/src/_auth/forms/DoctorSignupForm.jsx
@@ -5,7 +5,10 @@ import { useNavigate, Link } from "react-router-dom";
 const DoctorSignupForm = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    name: "",
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
     dob: "",
     gender: "",
     phone: "",
@@ -32,85 +35,129 @@ const DoctorSignupForm = () => {
       // Handle errors, show error messages to the user
     }
 
-    console.log(formData)
+    console.log(formData);
   };
 
   return (
-    <div className=" flex justify-between h-screen">
+    <div className="flex justify-between h-screen">
       <div className="flex flex-col justify-center items-center gap-2 w-1/2">
-        <h1 className=" text-center font-bold text-[23px]">Doctor SignUp</h1>
-        <div className="flex flex-col items-center justify-center p-4 gap-2  w overflow-auto h-auto mx-[300px] font-semibold">
+        <h1 className="text-center font-bold text-[23px]">Doctor SignUp</h1>
+        <div className="flex flex-col items-center justify-center p-4 gap-2 w overflow-auto h-auto mx-[300px] font-semibold">
           <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-[500px]">
-            
-              <div className="input-group">
-                <label htmlFor="name" className="text-[15px]">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
-                  placeholder="Enter Your Name"
-                  required
-                />
-              </div>
-
-            
-
-            <div className=" flex gap-2">
-              <div className="input-group">
-                <label htmlFor="gender" className="text-[15px]">
-                  Gender
-                </label>
-                <select
-                  name="gender"
-                  id="gender"
-                  value={formData.gender}
-                  onChange={handleChange}
-                  className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
-                  required
-                >
-                  <option value="">Select Gender</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-              <div className="input-group">
-                <label htmlFor="dob" className="text-[15px]">
-                  Date of Birth
-                </label>
-                <input
-                  type="date"
-                  name="dob"
-                  id="dob"
-                  value={formData.dob}
-                  onChange={handleChange}
-                  className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
-                  required
-                />
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="phone" className="text-[15px]">
-                  Phone
-                </label>
-                <input
-                  type="tel"
-                  name="phone"
-                  id="phone"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
-                  placeholder="Enter Your Phone Number"
-                  required
-                />
-              </div>
-
+            <div className="input-group">
+              <label htmlFor="firstName" className="text-[15px]">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter Your First Name"
+                required
+              />
             </div>
+
+            <div className="input-group">
+              <label htmlFor="lastName" className="text-[15px]">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter Your Last Name"
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="userName" className="text-[15px]">
+                User Name
+              </label>
+              <input
+                type="text"
+                name="userName"
+                id="userName"
+                value={formData.userName}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter Your User Name"
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="email" className="text-[15px]">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter Your Email"
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="dob" className="text-[15px]">
+                Date of Birth
+              </label>
+              <input
+                type="date"
+                name="dob"
+                id="dob"
+                value={formData.dob}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                required
+              />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="gender" className="text-[15px]">
+                Gender
+              </label>
+              <select
+                name="gender"
+                id="gender"
+                value={formData.gender}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                required
+              >
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="phone" className="text-[15px]">
+                Phone
+              </label>
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Enter Your Phone Number"
+                required
+              />
+            </div>
+
             <div className="input-group">
               <label htmlFor="medicalLicense" className="text-[15px]">
                 Medical License
@@ -126,6 +173,7 @@ const DoctorSignupForm = () => {
                 required
               />
             </div>
+
             <div className="input-group">
               <label htmlFor="specialization" className="text-[15px]">
                 Specialization
@@ -141,6 +189,7 @@ const DoctorSignupForm = () => {
                 required
               />
             </div>
+
             <div className="input-group">
               <label htmlFor="clinic" className="text-[15px]">
                 Clinic
@@ -156,6 +205,7 @@ const DoctorSignupForm = () => {
                 required
               />
             </div>
+
             <div className="input-group">
               <label htmlFor="yearsOfExperience" className="text-[15px]">
                 Years of Experience
@@ -171,6 +221,7 @@ const DoctorSignupForm = () => {
                 required
               />
             </div>
+
             <div className="input-group">
               <label htmlFor="medicalQualifications" className="text-[15px]">
                 Medical Qualifications
@@ -185,6 +236,7 @@ const DoctorSignupForm = () => {
                 required
               ></textarea>
             </div>
+
             <button
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
