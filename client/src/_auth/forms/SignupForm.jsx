5dc0bcb39f03a09b3d5a76e4c0c7740bfc70b8c0
Vineetchelani1304 <vineetchelani545@gmail.com>
2024-03-15 20:08:53 +0530

    register form

--- client/src/_auth/forms/SignupForm.jsx
+++ client/src/_auth/forms/SignupForm.jsx
@@ -7,41 +7,59 @@ import { useDispatch } from "react-redux";
 // import { setUser } from "../../store/userSlice";
 // import { useSelector } from "react-redux";
 
-const SignupForm = () => {
+const LoginForm = () => {
   const [location, setLocation] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [position, setPosition] = useState({ latitude: null, longitude: null });
 
   useEffect(() => {
-    const handleSearch = async () => {
-      try {
-        const { coords } = await getCurrentLocation();
-        const { latitude, longitude } = coords;
-        setLocation({ latitude, longitude });
-      } catch (error) {
-        console.error("Error searching:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const getCurrentLocation = async () => {
-      return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
       });
-    };
+    } else {
+      console.log("Geolocation is not available in your browser.");
+    }
+  }, []);
 
-    handleSearch();
-  }, []); // Empty dependency array ensures the effect runs only once
+  // useEffect(() => {
+  //   const handleSearch = async () => {
+  //     try {
+  //       const { coords } = await getCurrentLocation();
+  //       const { latitude, longitude } = coords;
+  //       setLocation({ latitude, longitude });
+  //     } catch (error) {
+  //       console.error("Error searching:", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-  console.log(location);
+  //   const getCurrentLocation = async () => {
+  //     return new Promise((resolve, reject) => {
+  //       navigator.geolocation.getCurrentPosition(resolve, reject);
+  //     });
+  //   };
+
+  //   handleSearch();
+  // }, []); // Empty dependency array ensures the effect runs only once
+
+  console.log(location, position);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+
   const [formData, setFormData] = useState({
     username: "",
     password: "",
+    confpassword:"",
     latitude: "",
     longitude: "",
     remember: false,
+    email:"",
+    phone:"",
   });
 
   const handleChange = (e) => {
@@ -51,113 +69,181 @@ const SignupForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(formData);
+    formData.latitude = position.latitude;
+    formData.longitude = position.longitude;
     await axios
       .post("http://localhost:8000/api/login", formData)
       .then((res) => {
         console.log(res.data);
         const { username, email } = res.data;
         console.log(username, email);
-        dispatch(setUser({ username, email }));
+        // dispatch(setUser({ username, email }));
         localStorage.setItem("username", username);
         console.log(localStorage.getItem("username"));
         console.log(res.data.refresh_token, res.data.access_token);
         if (res.data.access_token && res.data.refresh_token) {
           Cookie.set("refreshToken", res.data.refresh_token);
           Cookie.set("accessToken", res.data.access_token);
-          navigate("/dashboard");
+          navigate("/overview");
         }
       })
       .catch((err) => {
         console.log(err);
       });
+
+      console.log(formData);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-4 gap-2 overflow-auto h-full">
-      <img src="" alt="" />
-      <div className="bg-blue-500 rounded-3xl w-[12rem] text-white py-2 flex flex-row justify-center">
-        <button className="hover:text-black p-1 m-0.5 hover:rounded-2xl py-1 hover:bg-white">
-          Patient
-        </button>
-        <button className="hover:text-black p-1 m-0.5 hover:rounded-2xl py-1 hover:bg-white">
-          Doctor
-        </button>
-      </div>
-      <p className="text-[11px]">
-        Simplify appointments with AppointNext.io for efficient healthcare
-        scheduling.
-      </p>
-      <form action="" onSubmit={handleSubmit} className="flex flex-col gap-2">
-        <div className="input-group">
-          <label
-            htmlFor="username"
-            className={`transition-transform ${
-              formData.email ? "-translate-y-6 text-sm" : ""
-            } text-[15px]`}
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={formData.username}
-            onChange={handleChange}
-            className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
-            placeholder="Enter Your Username"
-          />
+    <div className="flex items-center justify-evenly h-screen gap-2 overflow-auto flex-row w-screen">
+      <div className=" flex-col gap-2 w-1/2 p-20">
+        <div className="bg-[#4F46E5] rounded-3xl w-[12rem] text-white py-2 flex flex-row justify-center ml-[90px]">
+          <button className="hover:text-black p-1 m-0.5 hover:rounded-2xl py-1 hover:bg-white">
+            Patient
+          </button>
+          <button className="hover:text-black p-1 m-0.5 hover:rounded-2xl py-1 hover:bg-white">
+            Doctor
+          </button>
         </div>
-        <div className="input-group">
-          <div className="flex flex-row justify-between items-center">
+        <br />
+        <p className="text-[11px]">
+          Simplify appointments with AppointNext.io for efficient healthcare
+          scheduling.
+        </p>
+        <form action="" onSubmit={handleSubmit} className="flex flex-col gap-2">
+          <div className="input-group font-semibold">
             <label
-              htmlFor="password"
+              htmlFor="username"
               className={`transition-transform ${
-                formData.password ? "text-sm" : ""
-              } text-[15px] `}
+                formData.email ? "-translate-y-6 text-sm" : ""
+              } text-[15px]`}
             >
-              Password
+              Username
             </label>
-            <Link
-              to="/forgotpassword"
-              className="text-[10px] text-blue-500 hover:cursor-pointer  "
-            >
-              Forgot Password?
-            </Link>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter Your Username"
+            />
           </div>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
-            placeholder="Enter Your Password"
-          />
+          <div className="input-group font-semibold">
+            <label
+              htmlFor="email"
+              className={`transition-transform ${
+                formData.email ? "-translate-y-6 text-sm" : ""
+              } text-[15px]`}
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter Your Email" required
+            />
+          </div>
+          <div className="input-group font-semibold">
+            <label
+              htmlFor="phone"
+              className={`transition-transform ${
+                formData.email ? "-translate-y-6 text-sm" : ""
+              } text-[15px]`}
+            >
+              Contact
+            </label>
+            <input
+              type='tel'
+              name="phone"
+              id="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="input-group font-semibold">
+            <div className="flex flex-row justify-between items-center">
+              <label
+                htmlFor="password"
+                className={`transition-transform ${
+                  formData.password ? "text-sm" : ""
+                } text-[15px] `}
+              >
+                Password
+              </label>
+      
+            </div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter Your Password" required
+            />
+          </div>
+          <div className="input-group font-semibold">
+            <div className="flex flex-row justify-between items-center">
+              <label
+                htmlFor="confpassword"
+                className={`transition-transform ${
+                  formData.password ? "text-sm" : ""
+                } text-[15px] `}
+              >
+                Confirm Password
+              </label>
+            
+            </div>
+            <input
+              type="password"
+              name="confpassword"
+              id="confpassword"
+              value={formData.confpassword}
+              onChange={handleChange}
+              className="w-full py-1 px-3 mt-1 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter Confirm Password" required
+            />
+          </div>
+          <div className="input-group flex flex-row items-center">
+            <input
+              type="checkbox"
+              name="remember"
+              id="remember"
+              value={formData.remember}
+              onChange={handleChange}
+            />
+            <label htmlFor="remember" className="px-2 text-[15px]">
+              Remember Me
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="bg-[#4F46E5] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Sign Up
+          </button>
+        </form>
+        <br />
+        <br />
+        <div className=" text-center">
+          <p className="text-[15px]">
+            Don’t have an account yet? Register now, for free!
+          </p>
         </div>
-        <div className="input-group flex flex-row items-center">
-          <input
-            type="checkbox"
-            name="remember"
-            id="remember"
-            value={formData.remember}
-            onChange={handleChange}
-          />
-          <label htmlFor="remember" className="px-2 text-[10px]">
-            Remember Me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Login
-        </button>
-      </form>
-      <p className="text-[10px]">
-        Don’t have an account yet? Register now, for free!
-      </p>
+      </div>
+      <div className=" bg-[#003CD8] h-screen justify-center flex items-center w-1/2">
+        <img src="./public/image.png" alt="" className=" w-full" />
+      </div>
     </div>
   );
 };
 
-export default SignupForm;
+export default LoginForm;
