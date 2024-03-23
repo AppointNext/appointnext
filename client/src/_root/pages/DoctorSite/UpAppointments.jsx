5d1a0959db79dae671ede15d14b63077624ae806
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-03-23 19:58:59 +0530

    bug fix

--- client/src/_root/pages/DoctorSite/UpAppointments.jsx
+++ client/src/_root/pages/DoctorSite/UpAppointments.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+// import React from "react";
+// import UpcomingAppointCard from "../../../components/shared/UpcomingAppointCard";
+// import UpcomingAppointDoctorCard from "../../../components/shared/SharedDoctors/UpcommingAppointments";
+// const UpAppointments = () => {
+//   return (
+//     <div className=" ml-[200px] mt-4 px-4">
+//       <h1 className=" font-semibold text-[23px]">Upcomming Appointments</h1>
+//       <div className=" overflow-auto flex">
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//       </div>
+//       <div className=" overflow-auto flex">
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//         <UpcomingAppointDoctorCard />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default UpAppointments;
+
+import React from "react";
+import UpcomingAppointDoctorCard from "../../../components/shared/SharedDoctors/UpcommingAppointments";
 
 const UpAppointments = () => {
   return (
-    <div className=' ml-[200px]'>
-      
+    <div className="ml-[200px] mt-4 px-4 relative">
+      <h1 className="font-semibold text-[23px]">Upcoming Appointments</h1>
+      <div className="overflow-x-auto flex flex-no-wrap h-[310px]">
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+      </div>
+      <div className="overflow-x-auto flex flex-no-wrap">
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+        <UpcomingAppointDoctorCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UpAppointments
+export default UpAppointments;
